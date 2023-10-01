ee650efa3a4b9be5bc4601c4452cac4121719d28
Ibad Ur Razzaq <ibad00769@gmail.com>
2023-10-02 01:02:10 +0530

    Demo: Added Animations
    Signed-off-by: Ibad Ur Razzaq <ibad00769@gmail.com>

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -3,16 +3,18 @@ import leftImg from '../../Assets/left-view.jpg';
 import rightImg from '../../Assets/right-view.jpg';
 import centerImg from '../../Assets/center-view.jpg';
 import centerView from '../../Assets/center-frame.jpg';
+import './Display.css';
 
 const Display = () => {
+
     return (
         <Container style={{ width: "100%" }} className="justify-content-center d-flex">
-            <img src={leftImg} alt="sfsdf" />
-            <Container className="d-flex align-items-center position-relative">
+            <img src={leftImg} className="left-watch" alt="sfsdf" />
+            <Container className="d-flex align-items-center position-relative center-watch">
                 <img style={{ height: "fit-content" }} src={centerImg} alt="fsdf" />
                 <img src={centerView} className="position-absolute" style={{left: "17%", borderRadius: "25%"}} alt="fsdf" />
             </Container>
-            <img src={rightImg} alt="sdfds" />
+            <img src={rightImg} className="right-watch" alt="sdfds" />
         </Container>
     )
 }
